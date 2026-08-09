a38268556e1b12ef57a82997848e39e2bcfc979a
MD MUNNA <munna.codeexpert@gmail.com>
2026-08-09 23:27:27 +0600

    updatedddddddddd

--- core/context_processors.py
+++ core/context_processors.py
@@ -43,14 +43,29 @@ def admin_dashboard_metrics(request):
         return {}
 
     try:
+        import sys
+        import django
+        from django.conf import settings
         from django.contrib.auth.models import User
         from products.models import Product, Category
-        from blog.models import Post, BlogCategory
         from contactapp.models import ContactMessage
         from certificates.models import Certificate
+        from activitylog.models import UserSession
+        
+        # Try importing from optional modules safely
+        try:
+            from services.models import ServiceItem
+            total_services = ServiceItem.objects.count()
+        except ImportError:
+            total_services = 0
+            
+        try:
+            from about.models import TeamMember
+            total_team_members = TeamMember.objects.count()
+        except ImportError:
+            total_team_members = 0
 
         total_products = Product.objects.count()
-        total_posts = Post.objects.count()
         total_messages = ContactMessage.objects.count()
         total_certificates = Certificate.objects.count()
         total_users = User.objects.count()
@@ -64,23 +79,27 @@ def admin_dashboard_metrics(request):
                 'count': cat.products.count()
             })
 
-        blog_category_data = []
-        for cat in BlogCategory.objects.all():
-            blog_category_data.append({
-                'name': cat.name,
-                'count': cat.posts.count()
-            })
+        # Gather System Info
+        db_engine = settings.DATABASES.get('default', {}).get('ENGINE', '').split('.')[-1]
+        system_info = {
+            'django_version': django.get_version(),
+            'python_version': sys.version.split()[0],
+            'timezone': settings.TIME_ZONE,
+            'db_engine': db_engine.upper() if db_engine else 'SQLITE',
+            'debug_mode': settings.DEBUG,
+        }
 
         return {
             'admin_metrics': {
                 'total_products': total_products,
-                'total_posts': total_posts,
                 'total_messages': total_messages,
                 'total_certificates': total_certificates,
                 'total_users': total_users,
+                'total_services': total_services,
+                'total_team_members': total_team_members,
                 'recent_messages': recent_messages,
                 'category_data': category_data,
-                'blog_category_data': blog_category_data,
+                'system_info': system_info,
             }
         }
     except Exception as e:
